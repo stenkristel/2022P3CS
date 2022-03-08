ac2bdad28a6ada8bde0ff9a0408c1a8744c5d2bd
stenkristel <stenkristel2016@gmail.com>
2022-03-08 11:59:00 +0100

    Les0 en les1 aangemaakt

---
+++ Les0/Les1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace les0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
